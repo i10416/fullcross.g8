2278f83bbe7bf04cb8a743d34e387fc225e33718
i10416 <ito.yo16uh90616@gmail.com>
2022-05-19 01:55:21 +0900

    Update MUnit

--- src/main/g8/project/Deps.scala
+++ src/main/g8/project/Deps.scala
@@ -4,6 +4,6 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Deps {
   val munit = Def.setting(
     Seq(
-      "org.scalameta" %%% "munit" % "1.0.0-M1"
+      "org.scalameta" %%% "munit" % "1.0.0-M4"
     )
 }
